afbf93622faa36e5b9e8eecde23559c9556c324f
SAudrius <121971874+SAudrius@users.noreply.github.com>
2024-01-06 14:39:38 +0200

    Created routes for ordersRoute,shopItems, usersRoles

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const authRoute = require('./routes/v1/authRoutes');
-const shopItemsRouter = require('./routes/v1/shopItemsRoute');
+const shopItemsRouter = require('./routes/v1/shopItemsRoutes');
+const ordersRoute = require('./routes/v1/ordersRoutes');
+const usersRolesRouter = require('./routes/v1/usersRolesRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -14,6 +16,8 @@ app.use(express.json());
 // routes
 app.use('/v1/api/auth', authRoute);
 app.use('/v1/api/shop_items', shopItemsRouter);
+app.use('/v1/api/orders', ordersRoute);
+app.use('/v1/api/users_roles', usersRolesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
