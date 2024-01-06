564cf6278ea224dfac7cefaf0b751ad8ff3f1a95
SAudrius <121971874+SAudrius@users.noreply.github.com>
2024-01-06 09:50:27 +0200

    shopItemsRoutes added & ignore DS_Store

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const authRoute = require('./routes/v1/authRoutes');
+const shopItemsRouter = require('./routes/v1/shopItemsRoute');
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // routes
 app.use('/v1/api/auth', authRoute);
+app.use('/v1/api/shop_items', shopItemsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
